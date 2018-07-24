7a353b9b7ade34a7167d065854faba211d175f20
toandx <strangersuntoan@gmail.com>
2018-07-24 22:40:34 +0700

    first commit

---
+++ CallIntent/app/src/main/java/com/example/toandx/callintent/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.toandx.callintent;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view)
+    {
+        EditText text=(EditText) findViewById(R.id.editText);
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:"+text.getText().toString()));
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        startActivity(callIntent);
+    }
+}
